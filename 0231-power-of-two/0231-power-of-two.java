69764060db30d2bcd749a3bf7804d1821910ddad
KRISHNA SHARMA <123051328+krishnasharma8@users.noreply.github.com>
2024-08-19 23:07:47 +0530

    Time: 1 ms (41.25%), Space: 40.7 MB (59.97%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0){
+            return false;
+        }
+        if(n==1){
+            return true;
+        }
+        while(n!=1) {
+            if(n%2==0){
+                n=n/2;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
